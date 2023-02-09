3d1e7d3c2248603ed619bef942b636f88f715728
Aura Román <kyradiscord@gmail.com>
2023-02-09 22:19:13 +0100

    feat: added reddit command

--- src/lib/i18n/LanguageKeys/Common.ts
+++ src/lib/i18n/LanguageKeys/Common.ts
@@ -1,4 +1,3 @@
 import { FT, Value } from '@skyra/http-framework-i18n';
 
 export const Currency = FT<Value<number>>('common:currency');
-export const CurrencyFallback = FT<Value<number>>('common:currencyFallback');
