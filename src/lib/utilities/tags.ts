71874e715979feeb44d78a7ef634661bf51a8a7d
Aura Román <kyradiscord@gmail.com>
2023-02-27 19:20:14 +0100

    feat: better tag autocomplete, remove `- id` (#36)

--- src/lib/utilities/tags.ts
+++ src/lib/utilities/tags.ts
@@ -1,3 +1,4 @@
+import { cut } from '#lib/common/strings';
 import type { Tag, TagAlias } from '@prisma/client';
 import { isNullishOrEmpty } from '@sapphire/utilities';
 import { container } from '@skyra/http-framework';
@@ -14,18 +15,13 @@ export function searchTag(guildId: bigint, query?: string | null) {
 		where: isNullishOrEmpty(query)
 			? { guildId }
 			: { guildId, OR: [{ name: { startsWith: query } }, { aliases: { some: { name: { startsWith: query } } } }] },
-		select: { id: true, name: true },
+		select: { id: true, name: true, content: true },
 		take: 25
 	});
 }
 
 export function getTag<A>(guildId: bigint, query: string, aliases?: A): Promise<(A extends true ? FullTag : Tag) | null>;
 export function getTag(guildId: bigint, query: string, aliases = false) {
-	if (query.endsWith('\u200B')) {
-		const id = BigInt(query.slice(query.lastIndexOf(' ') + 1, -1));
-		return container.prisma.tag.findFirst({ where: { id, guildId }, include: aliases ? { aliases: true } : undefined });
-	}
-
 	return container.prisma.tag.findFirst({
 		where: { guildId, OR: [{ name: query }, { aliases: { some: { name: query } } }] },
 		include: aliases ? { aliases: true } : undefined
@@ -34,10 +30,10 @@ export function getTag(guildId: bigint, query: string, aliases = false) {
 
 type FullTag = Tag & { aliases: TagAlias[] };
 
-export function makeTagChoice(tag: Pick<Tag, 'id' | 'name'>) {
-	return { name: tag.name, value: `${tag.name} - ${tag.id}\u200B` } satisfies APIApplicationCommandOptionChoice;
+export function makeTagChoice(tag: Pick<Tag, 'id' | 'name' | 'content'>) {
+	return { name: cut(`${tag.name} — ${tag.content}`, 100), value: tag.name } satisfies APIApplicationCommandOptionChoice;
 }
 
-export function makeTagChoices(tags: readonly Pick<Tag, 'id' | 'name'>[]) {
+export function makeTagChoices(tags: readonly Pick<Tag, 'id' | 'name' | 'content'>[]) {
 	return tags.map((tag) => makeTagChoice(tag));
 }
