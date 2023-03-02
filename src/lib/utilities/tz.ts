809f97d52b0579b35cfe68b90a60fcb05fdda915
Aura Román <kyradiscord@gmail.com>
2023-03-02 13:16:43 +0100

    feat(timezone): add country search support

--- src/lib/utilities/tz.ts
+++ src/lib/utilities/tz.ts
@@ -1,16 +1,35 @@
 import { PathSrc } from '#lib/common/constants';
+import { cut } from '#lib/common/strings';
+import { toTitleCase } from '@sapphire/utilities';
 import { readFile } from 'node:fs/promises';
 
-const PathTimeZone = new URL('./generated/data/tz.json', PathSrc);
 const tz = new Map<string, TimeZone>();
 
 export let MinimumLength = 100;
-export let MaximumLength = 0;
-for (const entry of JSON.parse(await readFile(PathTimeZone, 'utf8')) as TimeZone[]) {
-	tz.set(entry.name.toLowerCase(), entry);
+export const MaximumLength = 100;
 
-	if (entry.name.length < MinimumLength) MinimumLength = entry.name.length;
-	if (entry.name.length > MaximumLength) MaximumLength = entry.name.length;
+{
+	const tzCountries = new Map<string, TimeZoneCountry>();
+
+	const PathTimeZoneCountry = new URL('./generated/data/tz-country-codes.json', PathSrc);
+	for (const entry of JSON.parse(await readFile(PathTimeZoneCountry, 'utf8')) as TimeZoneCountry[]) {
+		tzCountries.set(entry.code, {
+			code: entry.code.toLowerCase(),
+			name: entry.name.toLowerCase()
+		});
+	}
+
+	const PathTimeZone = new URL('./generated/data/tz.json', PathSrc);
+	for (const entry of JSON.parse(await readFile(PathTimeZone, 'utf8')) as RawTimeZone[]) {
+		const countries = entry.codes.map((code) => tzCountries.get(code)!);
+		tz.set(entry.name.toLowerCase(), {
+			name: entry.name,
+			countries,
+			full: cut(`${entry.name} (${countries.map((country) => toTitleCase(country.name)).join(', ')})`, MaximumLength)
+		});
+
+		if (entry.name.length < MinimumLength) MinimumLength = entry.name.length;
+	}
 }
 
 export function getTimeZone(id: string) {
@@ -23,13 +42,25 @@ export function searchTimeZone(id: string) {
 	id = id.toLowerCase();
 	const entries = [] as TimeZone[];
 	for (const [key, value] of tz.entries()) {
-		if (!key.includes(id)) continue;
+		if (!(key.includes(id) || value.countries.some((country) => country.code === id || country.name.includes(id)))) continue;
 		if (entries.push(value) === 25) break;
 	}
 
 	return entries;
 }
 
+interface RawTimeZone {
+	codes: string[];
+	name: string;
+}
+
 export interface TimeZone {
+	countries: TimeZoneCountry[];
+	name: string;
+	full: string;
+}
+
+export interface TimeZoneCountry {
+	code: string;
 	name: string;
 }
