76bfebf66dd7ffe92a3db47b3b9d43f1315cb5d1
Aura Román <kyradiscord@gmail.com>
2023-03-25 00:07:55 +0100

    fix(wikipedia): handle errors more carefully

--- src/lib/i18n/LanguageKeys/Commands/Wikipedia.ts
+++ src/lib/i18n/LanguageKeys/Commands/Wikipedia.ts
@@ -6,4 +6,7 @@ export const RootDescription = T('commands/wikipedia:description');
 
 export const OptionsInput = 'commands/wikipedia:optionsInput';
 export const NoResults = T('commands/wikipedia:noResults');
+export const AbortError = T('commands/wikipedia:abortError');
+export const UnknownResponse = T('commands/wikipedia:unknownResponse');
+export const UnknownError = T('commands/wikipedia:unknownError');
 export const InterWiki = FT<{ link: string }>('commands/wikipedia:interWiki');
