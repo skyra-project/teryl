ee2e68a4059de7b117aac0b8014b2fb1730457bb
Aura Román <kyradiscord@gmail.com>
2023-05-18 12:27:11 +0200

    refactor(youtube): send ephemeral select instead of blocking (#105)

--- src/interaction-handlers/youtube.ts
+++ src/interaction-handlers/youtube.ts
@@ -1,23 +1,24 @@
-import { LanguageKeys } from '#lib/i18n/LanguageKeys';
 import { getLinkFromSelectMenu } from '#lib/utilities/youtube';
 import { InteractionHandler, type Interactions } from '@skyra/http-framework';
-import { resolveUserKey } from '@skyra/http-framework-i18n';
 import type { Snowflake } from 'discord-api-types/globals';
 import { MessageFlags, type APIActionRowComponent, type APIStringSelectComponent } from 'discord-api-types/v10';
 
 export class UserHandler extends InteractionHandler {
 	public async run(interaction: Interaction, [id]: [Snowflake]) {
-		if (interaction.user.id !== id) {
-			const content = resolveUserKey(interaction, LanguageKeys.InteractionHandlers.YouTube.MismatchingUserId);
-			return interaction.reply({ content, flags: MessageFlags.Ephemeral });
-		}
-
 		const [value] = interaction.values;
 		const content = getLinkFromSelectMenu(value);
 		const row = interaction.message.components![0] as APIActionRowComponent<APIStringSelectComponent>;
 		const component = row.components[0];
 		const options = component.options.map((option) => ({ ...option, default: option.value === value }));
-		return interaction.update({ content, components: [{ ...row, components: [{ ...component, options }] }] });
+		if (interaction.user.id === id) {
+			return interaction.update({ content, components: [{ ...row, components: [{ ...component, options }] }] });
+		}
+
+		return interaction.reply({
+			content,
+			components: [{ ...row, components: [{ ...component, custom_id: `youtube.${interaction.user.id}`, options }] }],
+			flags: MessageFlags.Ephemeral
+		});
 	}
 }
 
--- src/lib/i18n/LanguageKeys/InteractionHandlers/YouTube.ts
+++
@@ -1,3 +0,0 @@
-import { T } from '@skyra/http-framework-i18n';
-
-export const MismatchingUserId = T('interaction-handlers/youtube:mismatchingUserId');
