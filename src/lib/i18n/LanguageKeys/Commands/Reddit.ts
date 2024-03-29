2f601f2b56beb5b7e99ad127b1a500030d8bd637
Jeroen Claassens <support@favware.tech>
2024-03-29 23:50:49 +0100

    feat: add reddit post subcommand
    Co-authored-by: Aura <kyradiscord@gmail.com>

--- src/lib/i18n/LanguageKeys/Commands/Reddit.ts
+++ src/lib/i18n/LanguageKeys/Commands/Reddit.ts
@@ -5,14 +5,20 @@ export const RootName = T('commands/reddit:name');
 export const RootDescription = T('commands/reddit:description');
 
 export const OptionsReddit = 'commands/reddit:optionsReddit';
+export const OptionsPost = 'commands/reddit:optionsPost';
+
+export const Subreddit = 'commands/reddit:subreddit';
+export const Post = 'commands/reddit:post';
 
 export const InvalidName = T('commands/reddit:invalidName');
+export const InvalidPostKey = T('commands/reddit:invalidPostKey');
 export const Banned = T('commands/reddit:banned');
-export const Post = FT<{ title: string; author: string; url: string }>('commands/reddit:post');
+export const PostResult = FT<{ title: string; author: string; url: string }>('commands/reddit:post');
 export const NoPosts = T('commands/reddit:noPosts');
 export const AllNsfw = T('commands/reddit:allNsfw');
 export const AllNsfl = T('commands/reddit:allNsfl');
 export const AbortError = T('commands/reddit:abortError');
+export const ParsePostException = T('commands/reddit:parsePostException');
 export const UnavailableErrorPrivate = T('commands/reddit:unavailableErrorPrivate');
 export const UnavailableErrorGoldOnly = T('commands/reddit:unavailableErrorGoldOnly');
 export const UnavailableErrorQuarantined = FT<{ reason: string }>('commands/reddit:unavailableErrorQuarantined');
